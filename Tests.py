0ff12011761941db3dc69b2af3f5eb1426859963
Luke Wiley <Luke Wiley>
2016-10-26 19:38:19 -0400

    first commit

---
+++ Tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+class HomePageTest(unittest.TestCase):
+
+    def setUp(self):
+        if os.name=='nt':
+            # You might need to put the path to chromedriver.exe in
+            # the following line as an argument to Chrome()
+            # like webdriver.Chrome("C:\Program Files\Chrome") for instance
+
+            self.browser = webdriver.Chrome()
+        else:
+            self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
